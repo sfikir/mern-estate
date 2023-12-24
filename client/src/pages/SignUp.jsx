9453f3e8c8e29e607f5e7b6f1032618d6ef2349c
sfikir <sfikir77@gmail.com>
2023-12-23 16:05:03 -0800

    work on functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignUp() {
+
+  const [formData, setFormData] =useState({})
+  const handleChange = (e) => {
+
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
   return (
-    <div className='p-3 mx-auto max-w-lg'>
-      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username'className='border p-3 rounded-lg ' id='username' />
-        <input type="text" placeholder='email'className='border p-3 rounded-lg ' id='email' />
-        <input type="text" placeholder='password'className='border p-3 rounded-lg ' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Sign up</button>
+    <div className="p-3 mx-auto max-w-lg">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg "
+          id="username"
+          onChange={handleChange}        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg "
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+          id="password"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
+          Sign up
+        </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span>Sign in</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
     </div>
