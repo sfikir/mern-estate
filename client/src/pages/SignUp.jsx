9ef7fbdc061f617ae0b94aa4b97b4ff34080bc66
sfikir <sfikir77@gmail.com>
2023-12-27 03:24:29 -0800

    Signup page functionality continue ..

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,25 +3,41 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export default function SignUp() {
-
-  const [formData, setFormData] =useState({})
+  const [formData, setFormData] = useState({});
   const handleChange = (e) => {
-
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application.json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
+  console.log(formData);
   return (
     <div className="p-3 mx-auto max-w-lg">
       <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg "
           id="username"
-          onChange={handleChange}        />
+          onChange={handleChange}
+          
+        />
         <input
           type="text"
           placeholder="email"
